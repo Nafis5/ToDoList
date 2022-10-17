26d4399d92c73b014eb53053fa6f0ee253ae9b25
Nafis5 <nfsmahdee@gmail.com>
2022-10-17 06:19:57 +0600

    navigtion drawer implemented

--- app/src/main/java/com/notes/keepnotes/MainActivity.java
+++ app/src/main/java/com/notes/keepnotes/MainActivity.java
@@ -1,14 +1,18 @@
 package com.notes.keepnotes;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -17,6 +21,7 @@ import android.widget.CheckBox;
 import android.widget.Toast;
 
 
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +35,10 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
     List<Note> notes;
     List<Long> deletelist=new ArrayList();
     NoteDatabase db;
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    MenuInflater inflater;
+    public NavigationView nav_view;
 
 
 
@@ -43,58 +52,56 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         setSupportActionBar(toolbar);
         toolbar.setBackgroundColor(Color.parseColor("#000000"));
         toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.nav_open, R.string.nav_close);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        nav_view = (NavigationView) findViewById(R.id.nav_view);
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         recylerview=findViewById(R.id.allNotesList);
         recylerview.setLayoutManager(new LinearLayoutManager(this));
-         db=new NoteDatabase(this);
+        db=new NoteDatabase(this);
         notes=db.getAllNotes();
         adapter=new Adapter(this,notes);
         recylerview.setAdapter(adapter);
 
 
 
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id==R.id.backupId){
+                    Toast.makeText(MainActivity.this,"backup",Toast.LENGTH_SHORT).show();
+                }
+                if(id==R.id.restoreId){
+                    Toast.makeText(MainActivity.this,"restore",Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
 
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater=getMenuInflater();
-        inflater.inflate(R.menu.add_menu,menu);
-        return  true;
-    }
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()==R.id.add){
-
-
-            Intent i=new Intent(this,addNote.class);
-            startActivity(i);
-
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
-        if(item.getItemId()==R.id.only_delete){
-            //revome selected items from db
-            //dishablehome buton
-            //set title to keep notes
-            //bring back old menu
-            //updateAdapter
 
-            for(int i=0;i<deletelist.size();i++){
+        if(item.getItemId()==R.id.only_delete) {
 
-                db.deleteNote(deletelist.get(i));
-            }
-            Toast.makeText(this,counter+" items deleted",Toast.LENGTH_SHORT).show();
-            clearActionMode();
-
-            notes=db.getAllNotes();
-            adapter.updateAdapter(notes);
-
-
-
-
-
+            deleteNotes();
         }
-         else if(item.getItemId()==android.R.id.home){
+        else if(item.getItemId()==android.R.id.home){
 
             clearActionMode();
         }
@@ -105,8 +112,8 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
     @Override
     public boolean onLongClick(View v) {
         toolbar.getMenu().clear();
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(false);
         toolbar.inflateMenu(R.menu.only_delete_menu);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setTitle("0 item selected");
         is_in_action_mode=true;
 
@@ -132,25 +139,52 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         }
     }
     public  void clearActionMode(){
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
         toolbar.setTitle("Simple Notes");
         toolbar.getMenu().clear();
-        toolbar.inflateMenu(R.menu.add_menu);
         counter=0;
         is_in_action_mode=false;
         adapter.notifyDataSetChanged();
         deletelist.clear();
+
+
+
 
     }
 
     @Override
     public void onBackPressed() {
         //super.onBackPressed();
-        finishAffinity();
-        System.exit(0);
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            finishAffinity();
+            System.exit(0);
+        }
 
     }
 
 
 
+    public void goToAddNote(View view) {
+        Intent i=new Intent(this,addNote.class);
+        startActivity(i);
+    }
+    void deleteNotes(){
+        //revome selected items from db
+        //dishablehome buton
+        //set title to keep notes
+        //bring back old menu
+        //updateAdapter
+        for(int i=0;i<deletelist.size();i++){
+
+            db.deleteNote(deletelist.get(i));
+        }
+        Toast.makeText(this,counter+" items deleted",Toast.LENGTH_SHORT).show();
+        clearActionMode();
+
+        notes=db.getAllNotes();
+        adapter.updateAdapter(notes);
+    }
 }
